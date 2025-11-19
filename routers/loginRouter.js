45606ba0d90ffb18a12009d43a5dccadfec3dba7
Jens Kristian Ruben Hansen <jkh@dgibyen.dk>
2025-11-19 15:08:13 +0100

    added is_active checks in login endpoint

--- routers/loginRouter.js
+++ routers/loginRouter.js
@@ -4,40 +4,46 @@ import supabase from '../util/supabaseClient.js'
 
 const router = Router();
 
-//TODO: Tilføj cookies/sessioner
-
 router.post("/api/login", async (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
 
-  const { data: users, error } = await supabase
-    .from("users")
-    .select("*")
-    .eq("email", email)
-    .limit(1);
+  try {
+    const { data: users, error } = await supabase
+      .from("users")
+      .select("*")
+      .eq("email", email)
+      .limit(1);
 
-  if (error) {
+    if (error) {
+      console.error(error);
+      return res.status(500).send({ error: "Database error" });
+    }
+
+    const user = users[0];
+
+    if (error || !user) {
+      return res.status(401).send({ error: "User not found" });
+    }
+
+    if (!user.is_active) {
+      return res.status(403).send({ error: "Account not activated" });
+    }
+
+    const isValid = await comparePasswords(password, user.password_hash);
+    if (!isValid) {
+      return res.status(401).send({ error: "Invalid password" });
+    }
+
+    req.session.userId = user.id;
+
+    res.status(200).send({ data: "Login successful" });
+  } catch (error) {
     console.error(error);
-    return res.status(500).send({ error: "Database error" });
+    res.status(500).send({ error: "Internal server error" });
   }
-
-  const user = users[0];
-
-  if (!user) {
-    return res.status(401).send({ data: "User not found" });
-  }
-
-  const matchingHashedPasswords = await comparePasswords(
-    password,
-    user.password_hash
-  );
-
-  if (!matchingHashedPasswords) {
-    return res.status(401).send({ data: "Invalid password" });
-  }
-
-  req.session.userId = user.id;
-  res.status(200).send({ data: "Welcome sir!" });
 });
+
+
 
 export default router;
