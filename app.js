6618f6d8c5a0ac1812fafbe0392c9abe19d4f0af
JensKristianRuben <Jenskristian@gmail.dk>
2025-11-25 21:45:40 +0100

    rewinded to old method

--- app.js
+++ app.js
@@ -22,9 +22,7 @@ const __dirname = dirname(__filename);
 
 
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, "public")));
-app.use(express.static(path.join(__dirname, "client/dist")));
-app.use(express.static(path.join(__dirname, "client/dist/assets")));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(cookieParser(process.env.SESSION_SECRET));
 
@@ -59,7 +57,7 @@ app.use(sessionRouter);
 app.use(activationPage);
 
 app.get("/{*splat}", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/dist/index.html"));
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 const PORT = Number(process.env.PORT);
